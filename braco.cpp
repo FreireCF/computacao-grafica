60ea0e26f7cf4550782da3c1625f3d8dda2bfbb0
FreireCF <clecioferreira011@gmail.com>
2026-05-21 13:59:29 -0300

    práticas com transf. geométricas

---
+++ braco.cpp
@@ -0,0 +1,83 @@
+//#include <GLUT/glut.h>
+#include <GL/glut.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int ombro = 0, cotovelo = 0;
+
+void display(void) {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glLoadIdentity();
+
+  glPushMatrix();{
+    glPushMatrix();
+      glRotatef(cotovelo, 0.0, 1.0, 0.0);    
+      glColor3f(0.5, 0.0, 0.0);
+      glScalef(2.0, 0.4, 1.0);
+      glutWireCube(1.0);                  
+    glPopMatrix();
+
+    glTranslatef(2.0, 0.0, 0.0);
+      glPushMatrix();
+      glRotatef(ombro, 0.0, 1.0, 0.0);    
+      //glTranslatef(1.0, 0.0, 0.0);
+      glColor3f(0.0, 0.0, 0.5);
+      glScalef(2.0, 0.4, 1.0);
+      glutWireCube(1.0);
+    glPopMatrix();
+  glPopMatrix();
+}
+  glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(75.0, w / (h * 1.0), 1.0, 20.0);
+  gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+    case 'a':
+      ombro = (ombro + 5) % 360;
+      printf("%d", ombro);
+      glutPostRedisplay();
+      break;
+    case 'd':
+      ombro = (ombro - 5) % 360;
+      printf("%d", ombro);
+      glutPostRedisplay();
+      break;
+    case 's':
+      cotovelo = (cotovelo + 5) % 360;
+      glutPostRedisplay();
+      break;
+    case 'w':
+      cotovelo = (cotovelo - 5) % 360;
+      glutPostRedisplay();
+      break;
+    case 27:
+      exit(0);
+      break;
+    default:
+      break;
+  }
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutCreateWindow(argv[0]);
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(keyboard);
+  glClearColor(1, 1, 1, 1);
+  glutMainLoop();
+  return 0;
+}
